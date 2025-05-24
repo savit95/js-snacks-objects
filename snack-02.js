519d1afbbb53f67b4e8d4b27b37b48491986961f
savit95 <salvatorevitiello95@gmail.com>
2025-05-24 11:47:45 +0200

    Completed exercise snack-02

---
+++ snack-02.js
@@ -0,0 +1,13 @@
+// A partire da un array di stringhe, crea un secondo array formattando 
+// le stringhe del primo array in minuscolo e con l'iniziale maiscuola.
+// Es: ['pippo','PLUTO','Paperino'] -> ['Pippo'.'Pluto','Paperino'].
+
+let arrayStringUno = ['pippo','PLUTO','Paperino','ToPoLiNo','tOPOliNa'];
+let arrayStringDue = [];
+
+for (let i = 0; i < arrayStringUno.length; i++) {
+    let string = arrayStringUno[i][0].toUpperCase() + arrayStringUno[i].slice(1).toLocaleLowerCase();
+    arrayStringDue.push(string);
+}
+console.log(arrayStringUno);
+console.log(arrayStringDue);
